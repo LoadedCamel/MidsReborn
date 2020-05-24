1e23815c86a8589b84b50a4ae70db8f943cfbb51
Crytilis <crytilis@gmail.com>
2020-05-24 11:59:47 -0500

    Fixed AttribMod import, thanks Feles for talking me through this!

--- Hero Designer/Forms/JsonImport/frmJsonImportMain.cs
+++ Hero Designer/Forms/JsonImport/frmJsonImportMain.cs
@@ -16,5 +16,42 @@ namespace Hero_Designer.Forms.JsonImport
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog1.FileName;
+                parseAttriModFile(FileName);
+            }
+        }
+
+        private void parseAttriModFile(string fileName)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            string json = File.ReadAllText(fileName);
+            List<Class> classes = JsonConvert.DeserializeObject<List<Class>>(json);
+
+            foreach (Class c in classes)
+            {
+                for (int i = 0; i < DatabaseAPI.Database.AttribMods.Modifier[0].Table.Length - 1; i++)
+                {
+                    for (int j = 0; j < DatabaseAPI.Database.Classes.Length - 1; j++)
+                    {
+                        for(int k = 0; k < c.ModTables[0].Values.Count -1; k++) { 
+                            if (j < c.ModTables[0].Values.Count - 1)
+                            {
+                                //Console.WriteLine("Old value {0}, new value {1}", DatabaseAPI.Database.AttribMods.Modifier[0].Table[i][j], c.ModTables[0].Values[j]);
+                                int tableId = DatabaseAPI.NidFromUidAttribMod(c.ModTables[k].Name);
+                                if (tableId > 0)
+                                    DatabaseAPI.Database.AttribMods.Modifier[tableId].Table[i][j] = c.ModTables[k].Values[j];
+                            }
+                        }
+                    }
+                }
+            }
+            DatabaseAPI.Database.AttribMods.Store(My.MyApplication.GetSerializer());
+            MessageBox.Show("Import completed");
+        }
     }
 }
