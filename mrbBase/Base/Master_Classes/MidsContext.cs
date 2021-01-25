087837944a434e3a5f597984616ac4d96ca43a4a
Jason Thompson <crytilis@gmail.com>
2021-01-25 16:28:30 -0600

    Version 3.0.4.6

--- mrbBase/Base/Master_Classes/MidsContext.cs
+++ mrbBase/Base/Master_Classes/MidsContext.cs
@@ -8,9 +8,9 @@ namespace mrbBase.Base.Master_Classes
         public const string AppName = "Mids' Reborn";
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 0;
-        private const int AppBuildVersion = 3;
-        private const int AppRevisionVersion = 2;
-        public const string AppAssemblyVersion = "3.0.3.2";
+        private const int AppBuildVersion = 4;
+        private const int AppRevisionVersion = 6;
+        public const string AppAssemblyVersion = "3.0.4.6";
         public const string AppVersionStatus = "";
 
         public const string Title = "Mids' Reborn";
