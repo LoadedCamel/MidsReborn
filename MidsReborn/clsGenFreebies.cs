a5f8f345c29eab279d5ab727b54e245eee37a8a7
zethriller <thriller.ze@gmail.com>
2021-11-21 03:57:02 +0100

    Fix send build to beta generating impossible IO levels (out of allowed level range)

--- MidsReborn/clsGenFreebies.cs
+++ MidsReborn/clsGenFreebies.cs
@@ -39,7 +39,7 @@ namespace Mids_Reborn
                     var setLevelMax = enhData.nIDSet > -1
                         ? DatabaseAPI.Database.EnhancementSets[enhData.nIDSet].LevelMax
                         : 49;
-                    var enhBoostLevel = 50;
+                    var enhBoostLevel = enhData.LevelMax + 1;
                     if (enhData.nIDSet > -1 & AutoAttune & setLevelMax < 49)
                     {
                         enhUid = enhData.UID.Replace("Crafted_", "Attuned_");
