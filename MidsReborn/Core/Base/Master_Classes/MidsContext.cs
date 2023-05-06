ba383a8372164ee37b395b112d3f837ed59a60f5
crytilis <crytilis@gmail.com>
2023-05-06 01:43:54 -0500

    Corrected mistake in revision

--- MidsReborn/Core/Base/Master_Classes/MidsContext.cs
+++ MidsReborn/Core/Base/Master_Classes/MidsContext.cs
@@ -9,7 +9,7 @@ namespace Mids_Reborn.Core.Base.Master_Classes
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 5;
         private const int AppBuildVersion = 4;
-        private const int AppRevisionVersion = 1;
+        private const int AppRevisionVersion = 11;
 
         public const string AssemblyVersion = "3.5.4";
         public const string AssemblyFileVersion = "3.5.4.11";
