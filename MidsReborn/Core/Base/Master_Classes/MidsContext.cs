90f2d9285bd9fa6fca87240cfe45022a08576265
crytilis <crytilis@gmail.com>
2023-05-07 14:59:16 -0500

    Bumped App Version to 3.5.5.4 to (5th build 4th revision)

--- MidsReborn/Core/Base/Master_Classes/MidsContext.cs
+++ MidsReborn/Core/Base/Master_Classes/MidsContext.cs
@@ -8,11 +8,11 @@ namespace Mids_Reborn.Core.Base.Master_Classes
         public const string AppName = "Mids' Reborn";
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 5;
-        private const int AppBuildVersion = 4;
-        private const int AppRevisionVersion = 12;
+        private const int AppBuildVersion = 5;
+        private const int AppRevisionVersion = 4;
 
-        public const string AssemblyVersion = "3.5.4";
-        public const string AssemblyFileVersion = "3.5.4.12";
+        public const string AssemblyVersion = "3.5.5";
+        public const string AssemblyFileVersion = "3.5.5.4";
         public static Version AppFileVersion { get; set; } = new(AppMajorVersion, AppMinorVersion, AppBuildVersion, AppRevisionVersion);
 
         public const string AppVersionStatus = "";
