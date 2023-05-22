254c94c08802dc282c769483bc0365fed190e357
crytilis <crytilis@gmail.com>
2023-05-22 16:29:32 -0500

    frmMain: Offloaded form API overrides to WinApi class, increased loading speed and removed animation.
    Added a testbed for new controls.

---
+++ MidsReborn/UIv2/ControlTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mids_Reborn.UIv2
+{
+    public partial class ControlTest : Form
+    {
+        public ControlTest()
+        {
+            InitializeComponent();
+        }
+
+        private void enhPicker1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
