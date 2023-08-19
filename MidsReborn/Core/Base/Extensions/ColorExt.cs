c99c4bfbbd344bd5d880b0e0023374318ee52993
zethriller <thriller.ze@gmail.com>
2023-08-19 13:34:51 +0200

    Use alpha in html hex colors only if < 255

--- MidsReborn/Core/Base/Extensions/ColorExt.cs
+++ MidsReborn/Core/Base/Extensions/ColorExt.cs
@@ -6,8 +6,7 @@ namespace Mids_Reborn.Core.Base.Extensions
     {
         public static string ToHex(this Color color, bool sharpPrefix=true)
         {
-            //return $@"{(sharpPrefix ? "#" : "")}{ColorTranslator.ToWin32(color):X8}";
-            return $@"{(sharpPrefix ? "#" : "")}{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}";
+            return $@"{(sharpPrefix ? "#" : "")}{color.R:X2}{color.G:X2}{color.B:X2}{(color.A < 255 ? $"{color.A:X2}" : "")}";
         }
     }
 }
