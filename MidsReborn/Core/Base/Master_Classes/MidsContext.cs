9b8c35a48ebe02998faf0f6e695b1b808b2bbf4f
crytilis <crytilis@gmail.com>
2023-08-28 10:15:01 -0500

    113+ total commits ver changed to 3.6.5.3

--- MidsReborn/Core/Base/Master_Classes/MidsContext.cs
+++ MidsReborn/Core/Base/Master_Classes/MidsContext.cs
@@ -8,11 +8,11 @@ namespace Mids_Reborn.Core.Base.Master_Classes
         public const string AppName = "Mids' Reborn";
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 6;
-        private const int AppBuildVersion = 2;
-        private const int AppRevisionVersion = 7;
+        private const int AppBuildVersion = 5;
+        private const int AppRevisionVersion = 3;
 
-        public const string AssemblyVersion = "3.6.2";
-        public const string AssemblyFileVersion = "3.6.2.7";
+        public const string AssemblyVersion = "3.6.5";
+        public const string AssemblyFileVersion = "3.6.5.3";
         public static Version AppFileVersion { get; set; } = new(AppMajorVersion, AppMinorVersion, AppBuildVersion, AppRevisionVersion);
 
         public const string AppVersionStatus = "";
