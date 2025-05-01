faa76b68859e042dd7cf0bd73be216708296db73
Jason Thompson <18664232+Metalios@users.noreply.github.com>
2025-05-01 18:58:55 -0500

    Rev bump 3.7.6.14

--- MidsReborn/Core/Base/Master_Classes/MidsContext.cs
+++ MidsReborn/Core/Base/Master_Classes/MidsContext.cs
@@ -9,9 +9,9 @@ namespace Mids_Reborn.Core.Base.Master_Classes
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 7;
         private const int AppBuildVersion = 6;
-        private const int AppRevisionVersion = 13;
+        private const int AppRevisionVersion = 14;
         public const string AssemblyVersion = "3.7.6";
-        public const string AssemblyFileVersion = "3.7.6.13";
+        public const string AssemblyFileVersion = "3.7.6.14";
       
         public static Version AppFileVersion { get; set; } = new(AppMajorVersion, AppMinorVersion, AppBuildVersion, AppRevisionVersion);
 
