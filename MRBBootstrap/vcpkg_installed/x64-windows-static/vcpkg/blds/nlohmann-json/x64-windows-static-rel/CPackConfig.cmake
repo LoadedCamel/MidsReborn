1f3b91a82e3d4e9a367ad6e77e409e4d60b6feff
Jason Thompson <18664232+Metalios@users.noreply.github.com>
2025-06-15 20:10:18 -0500

    Added vcpkg manifest for MRBBootstrap and associated packages.

---
+++ MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel/CPackConfig.cmake
@@ -0,0 +1,71 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BINARY_7Z "OFF")
+set(CPACK_BINARY_IFW "OFF")
+set(CPACK_BINARY_INNOSETUP "OFF")
+set(CPACK_BINARY_NSIS "ON")
+set(CPACK_BINARY_NUGET "OFF")
+set(CPACK_BINARY_WIX "OFF")
+set(CPACK_BINARY_ZIP "OFF")
+set(CPACK_BUILD_SOURCE_DIRS "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/src/v3.12.0-1a1eb3c42a.clean;I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel")
+set(CPACK_CMAKE_GENERATOR "Ninja")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "C:/Users/jason/AppData/Local/vcpkg/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/share/cmake-3.30/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "nlohmann_json built using CMake")
+set(CPACK_GENERATOR "NSIS")
+set(CPACK_INNOSETUP_ARCHITECTURE "x64")
+set(CPACK_INSTALL_CMAKE_PROJECTS "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel;nlohmann_json;ALL;/")
+set(CPACK_INSTALL_PREFIX "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/pkgs/nlohmann-json_x64-windows-static")
+set(CPACK_MODULE_PATH "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/src/v3.12.0-1a1eb3c42a.clean/cmake")
+set(CPACK_NSIS_DISPLAY_NAME "nlohmann_json 3.12.0")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+set(CPACK_NSIS_PACKAGE_NAME "nlohmann_json 3.12.0")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "C:/Users/jason/AppData/Local/vcpkg/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/share/cmake-3.30/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "nlohmann_json built using CMake")
+set(CPACK_PACKAGE_FILE_NAME "nlohmann_json-3.12.0-win64")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "nlohmann_json 3.12.0")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "nlohmann_json 3.12.0")
+set(CPACK_PACKAGE_NAME "nlohmann_json")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Humanity")
+set(CPACK_PACKAGE_VERSION "3.12.0")
+set(CPACK_PACKAGE_VERSION_MAJOR "3")
+set(CPACK_PACKAGE_VERSION_MINOR "12")
+set(CPACK_PACKAGE_VERSION_PATCH "0")
+set(CPACK_PRODUCTBUILD_DOMAINS "ON")
+set(CPACK_RESOURCE_FILE_LICENSE "C:/Users/jason/AppData/Local/vcpkg/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/share/cmake-3.30/Templates/CPack.GenericLicense.txt")
+set(CPACK_RESOURCE_FILE_README "C:/Users/jason/AppData/Local/vcpkg/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/share/cmake-3.30/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "C:/Users/jason/AppData/Local/vcpkg/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/share/cmake-3.30/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_7Z "ON")
+set(CPACK_SOURCE_GENERATOR "7Z;ZIP")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_ZIP "ON")
+set(CPACK_SYSTEM_NAME "win64")
+set(CPACK_THREADS "1")
+set(CPACK_TOPLEVEL_TAG "win64")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "I:/Repos/Crytilis/MidsReborn/MRBBootstrap/vcpkg_installed/x64-windows-static/vcpkg/blds/nlohmann-json/x64-windows-static-rel/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
