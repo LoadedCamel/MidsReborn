a2987029c9e19c629f76ad55142b818e450afe62
zethriller <thriller.ze@gmail.com>
2025-07-06 01:47:28 +0200

    Add mutex check on prestige power selection for both columns

--- MidsReborn/Forms/WindowMenuItems/frmPrestige.cs
+++ MidsReborn/Forms/WindowMenuItems/frmPrestige.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
@@ -44,7 +43,7 @@ namespace Mids_Reborn.Forms.WindowMenuItems
             Load += frmPrestige_Load;
             _locked = false;
             InitializeComponent();
-            var componentResourceManager = new ComponentResourceManager(typeof(frmPrestige));
+            //var componentResourceManager = new ComponentResourceManager(typeof(frmPrestige));
             Icon = Resources.MRB_Icon_Concept;
             Name = nameof(frmPrestige);
             _myParent = iParent;
@@ -106,23 +105,21 @@ namespace Mids_Reborn.Forms.WindowMenuItems
         private void ChangedScrollFrameContents()
         {
             VScrollBar1.Value = 0;
-            VScrollBar1.Maximum =
-                (int) Math.Round(PopInfo.lHeight * (VScrollBar1.LargeChange / (double) Panel1.Height));
+            VScrollBar1.Maximum = (int)Math.Round(PopInfo.lHeight * (VScrollBar1.LargeChange / (double) Panel1.Height));
             VScrollBar1_Scroll(VScrollBar1, new ScrollEventArgs(ScrollEventType.EndScroll, 0));
         }
 
         private void FillLists()
         {
-            _myPowers = _myPowers.OrderBy(x => x.DisplayName).ToList();
+            _myPowers = _myPowers.OrderBy(x => x?.DisplayName).ToList();
             llLeft.SuspendRedraw = true;
             llRight.SuspendRedraw = true;
             llLeft.ClearItems();
             llRight.ClearItems();
-            var num = _myPowers.Count - 1;
-            for (var index = 0; index <= num; ++index)
+            for (var index = 0; index < _myPowers.Count; index++)
             {
                 var iState = !MidsContext.Character.CurrentBuild.PowerUsed(_myPowers[index])
-                    ? !((_myPowers[index].PowerType != Enums.ePowerType.Click) | _myPowers[index].ClickBuff)
+                    ? !(_myPowers[index].PowerType != Enums.ePowerType.Click | _myPowers[index].ClickBuff)
                         ? !_myPowers[index].SubIsAltColor ? ListLabel.LlItemState.Disabled :
                         ListLabel.LlItemState.Invalid
                         : ListLabel.LlItemState.Enabled
@@ -131,10 +128,14 @@ namespace Mids_Reborn.Forms.WindowMenuItems
                     ? new ListLabel.ListLabelItem(_myPowers[index].DisplayName, iState)
                     : new ListLabel.ListLabelItem(_myPowers[index].DisplayName, iState, -1, -1, -1, "",
                         ListLabel.LlFontFlags.Bold);
-                if (index >= _myPowers.Count / 2.0)
+                if (index >= _myPowers.Count / 2f)
+                {
                     llRight.AddItem(iItem);
+                }
                 else
+                {
                     llLeft.AddItem(iItem);
+                }
             }
 
             llLeft.SuspendRedraw = false;
@@ -183,55 +184,71 @@ namespace Mids_Reborn.Forms.WindowMenuItems
             lblLock.Visible = false;
         }
 
-        private void llLeft_ItemClick(ListLabel.ListLabelItem Item, MouseButtons Button)
+        private void ListItemClick(ListLabel sender, ListLabel.ListLabelItem item, MouseButtons button)
         {
-            var pIDX = Item.Index;
-            if (Button == MouseButtons.Right)
+            var pIDX = sender.Name == "llRight" ? item.Index + llLeft.Items.Length : item.Index;
+            if (button == MouseButtons.Right)
             {
                 _locked = false;
-                MiniPowerInfo(Item.Index);
+                MiniPowerInfo(item.Index);
                 lblLock.Visible = true;
                 _locked = true;
+
+                return;
+            }
+            
+            if (item.ItemState == ListLabel.LlItemState.Disabled)
+            {
+                return;
+            }
+
+            if (MidsContext.Character.CurrentBuild.PowerUsed(_myPowers[pIDX]))
+            {
+                MidsContext.Character.CurrentBuild.RemovePower(_myPowers[pIDX]);
+                item.ItemState = ListLabel.LlItemState.Enabled;
             }
             else
             {
-                if (Item.ItemState == ListLabel.LlItemState.Disabled)
-                    return;
-                if (MidsContext.Character.CurrentBuild.PowerUsed(_myPowers[pIDX]))
-                {
-                    MidsContext.Character.CurrentBuild.RemovePower(_myPowers[pIDX]);
-                    Item.ItemState = ListLabel.LlItemState.Enabled;
-                }
-                else
-                {
-                    // Toggle on if (any):
-                    // - Toggle default is ON
-                    // - Is a click buff
-                    // - Is an auto
-                    var pToggled =
-                        (_myPowers[pIDX].AlwaysToggle & _myPowers[pIDX].PowerType == Enums.ePowerType.Toggle) |
-                        _myPowers[pIDX].ClickBuff |
-                        _myPowers[pIDX].PowerType == Enums.ePowerType.Auto_;
-                    var p = MidsContext.Character.CurrentBuild.AddPower(_myPowers[pIDX]);
-                    
-                    // Get power index in build powers' list
-                    var hIDPower = MidsContext.Character.CurrentBuild.Powers.FindIndex(e => e.Power != null && e.Power.StaticIndex == p.Power.StaticIndex);
-                    // Check for mutexes
-                    var eMutex = MainModule.MidsController.Toon.CurrentBuild.MutexV2(hIDPower);
-                    MidsContext.Character.CurrentBuild.Powers[hIDPower].StatInclude = (eMutex == Enums.eMutex.NoConflict) | (eMutex == Enums.eMutex.NoGroup) && pToggled;
-                    
-                    Item.ItemState = ListLabel.LlItemState.Selected;
-                }
+                // Toggle on if (any):
+                // - Toggle default is ON
+                // - Is a click buff
+                // - Is an auto
+                var pToggled =
+                    _myPowers[pIDX].AlwaysToggle & _myPowers[pIDX].PowerType == Enums.ePowerType.Toggle |
+                    _myPowers[pIDX].ClickBuff |
+                    _myPowers[pIDX].PowerType == Enums.ePowerType.Auto_;
+                var p = MidsContext.Character.CurrentBuild.AddPower(_myPowers[pIDX]);
 
-                llLeft.Refresh();
-                _myParent.PowerModified(true);
-                _myParent.DoRefresh();
+                // Get power index in build powers' list
+                var hIDPower = MidsContext.Character.CurrentBuild.Powers.FindIndex(e => e is { Power: not null } && e.Power.StaticIndex == p.Power?.StaticIndex);
+                // Check for mutexes
+                var eMutex = MainModule.MidsController.Toon.CurrentBuild.MutexV2(hIDPower);
+                MidsContext.Character.CurrentBuild.Powers[hIDPower].StatInclude = eMutex == Enums.eMutex.NoConflict | eMutex == Enums.eMutex.NoGroup && pToggled;
+
+                item.ItemState = ListLabel.LlItemState.Selected;
             }
+
+            if (sender.Name == "llRight")
+            {
+                llRight.Refresh();
+            }
+            else
+            {
+                llLeft.Refresh();
+            }
+
+            _myParent.PowerModified(true);
+            _myParent.DoRefresh();
         }
 
-        private void llLeft_ItemHover(ListLabel.ListLabelItem Item)
+        private void llLeft_ItemClick(ListLabel.ListLabelItem item, MouseButtons button)
         {
-            MiniPowerInfo(Item.Index);
+            ListItemClick(llLeft, item, button);
+        }
+
+        private void llLeft_ItemHover(ListLabel.ListLabelItem item)
+        {
+            MiniPowerInfo(item.Index);
         }
 
         private void llLeft_MouseEnter(object sender, EventArgs e)
@@ -241,40 +258,14 @@ namespace Mids_Reborn.Forms.WindowMenuItems
             Panel2.Focus();
         }
 
-        private void llRight_ItemClick(ListLabel.ListLabelItem Item, MouseButtons Button)
+        private void llRight_ItemClick(ListLabel.ListLabelItem item, MouseButtons button)
         {
-            var pIDX = Item.Index + llLeft.Items.Length;
-            if (Button == MouseButtons.Right)
-            {
-                _locked = false;
-                MiniPowerInfo(pIDX);
-                lblLock.Visible = true;
-                _locked = true;
-            }
-            else
-            {
-                if (Item.ItemState == ListLabel.LlItemState.Disabled)
-                    return;
-                if (MidsContext.Character.CurrentBuild.PowerUsed(_myPowers[pIDX]))
-                {
-                    MidsContext.Character.CurrentBuild.RemovePower(_myPowers[pIDX]);
-                    Item.ItemState = ListLabel.LlItemState.Enabled;
-                }
-                else
-                {
-                    MidsContext.Character.CurrentBuild.AddPower(_myPowers[pIDX]).StatInclude = true;
-                    Item.ItemState = ListLabel.LlItemState.Selected;
-                }
-
-                llRight.Refresh();
-                _myParent.PowerModified(false);
-                _myParent.DoRefresh();
-            }
+            ListItemClick(llRight, item, button);
         }
 
-        private void llRight_ItemHover(ListLabel.ListLabelItem Item)
+        private void llRight_ItemHover(ListLabel.ListLabelItem item)
         {
-            MiniPowerInfo(Item.Index + llLeft.Items.Length);
+            MiniPowerInfo(item.Index + llLeft.Items.Length);
         }
 
         private void llRight_MouseEnter(object sender, EventArgs e)
@@ -285,7 +276,10 @@ namespace Mids_Reborn.Forms.WindowMenuItems
         private void MiniPowerInfo(int pIDX)
         {
             if (_locked)
+            {
                 return;
+            }
+
             IPower? power1 = new Power(_myPowers[pIDX]);
             var iPopup = new PopUp.PopupData();
             if (pIDX < 0)
@@ -299,8 +293,8 @@ namespace Mids_Reborn.Forms.WindowMenuItems
                 var str = string.Empty;
                 switch (power1.PowerType)
                 {
-                    case Enums.ePowerType.Click:
-                        if (power1.ClickBuff) str = "(Click)";
+                    case Enums.ePowerType.Click when power1.ClickBuff:
+                        str = "(Click)";
                         break;
                     case Enums.ePowerType.Auto_:
                         str = "(Auto)";
@@ -311,69 +305,92 @@ namespace Mids_Reborn.Forms.WindowMenuItems
                 }
 
                 iPopup.Sections[index1].Add(power1.DisplayName, PopUp.Colors.Title);
-                iPopup.Sections[index1].Add(str + " " + power1.DescShort, PopUp.Colors.Text, 0.9f);
+                iPopup.Sections[index1].Add($"{str} {power1.DescShort}", PopUp.Colors.Text, 0.9f);
                 var index2 = iPopup.Add();
-                if (power1.EndCost > 0.0)
+                if (power1.EndCost > 0)
                 {
-                    if (power1.ActivatePeriod > 0.0)
-                        iPopup.Sections[index2].Add("End Cost:", PopUp.Colors.Title,
-                            Utilities.FixDP(power1.EndCost / power1.ActivatePeriod) + "/s", PopUp.Colors.Title, 0.9f,
-                            FontStyle.Bold, 1);
-                    else
-                        iPopup.Sections[index2].Add("End Cost:", PopUp.Colors.Title, Utilities.FixDP(power1.EndCost),
-                            PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
+                    iPopup.Sections[index2].Add("End Cost:", PopUp.Colors.Title,
+                        power1.ActivatePeriod > 0
+                            ? $"{Utilities.FixDP(power1.EndCost / power1.ActivatePeriod)}/s"
+                            : Utilities.FixDP(power1.EndCost), PopUp.Colors.Title, 0.9f,
+                        FontStyle.Bold, 1);
                 }
 
-                if ((power1.EntitiesAutoHit == Enums.eEntity.None) | ((power1.Range > 20.0) &
-                                                                      power1.I9FXPresentP(Enums.eEffectType.Mez,
-                                                                          Enums.eMez.Taunt)))
+                if (power1.EntitiesAutoHit == Enums.eEntity.None | power1.Range > 20 &
+                    power1.I9FXPresentP(Enums.eEffectType.Mez, Enums.eMez.Taunt))
+                {
                     iPopup.Sections[index2].Add("Accuracy:", PopUp.Colors.Title,
-                        Utilities.FixDP((float) (MidsContext.Config.ScalingToHit * (double) power1.Accuracy * 100.0)) + "%",
+                        $"{Utilities.FixDP(MidsContext.Config.ScalingToHit * power1.Accuracy * 100)}%",
                         PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
-                if (power1.RechargeTime > 0.0)
+                }
+
+                if (power1.RechargeTime > 0)
+                {
                     iPopup.Sections[index2].Add("Recharge:", PopUp.Colors.Title,
-                        Utilities.FixDP(power1.RechargeTime) + "s", PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
+                        $"{Utilities.FixDP(power1.RechargeTime)}s", PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
+                }
+
                 var durationEffectId = power1.GetDurationEffectID();
-                var iNum = 0.0f;
+                var iNum = 0f;
                 if (durationEffectId > -1)
+                {
                     iNum = power1.Effects[durationEffectId].Duration;
-                if ((power1.PowerType != Enums.ePowerType.Toggle) & (power1.PowerType != Enums.ePowerType.Auto_) &&
-                    iNum > 0.0)
-                    iPopup.Sections[index2].Add("Duration:", PopUp.Colors.Title, Utilities.FixDP(iNum) + "s",
+                }
+
+                if (power1.PowerType != Enums.ePowerType.Toggle & power1.PowerType != Enums.ePowerType.Auto_ && iNum > 0)
+                {
+                    iPopup.Sections[index2].Add("Duration:", PopUp.Colors.Title, $"{Utilities.FixDP(iNum)}s",
                         PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
-                if (power1.Range > 0.0)
-                    iPopup.Sections[index2].Add("Range:", PopUp.Colors.Title, Utilities.FixDP(power1.Range) + "ft",
+                }
+
+                if (power1.Range > 0)
+                {
+                    iPopup.Sections[index2].Add("Range:", PopUp.Colors.Title, $"{Utilities.FixDP(power1.Range)}ft",
                         PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
+                }
+
                 if (power1.Arc > 0)
-                    iPopup.Sections[index2].Add("Arc:", PopUp.Colors.Title, Convert.ToString(power1.Arc) + "°",
+                {
+                    iPopup.Sections[index2].Add("Arc:", PopUp.Colors.Title, $"{power1.Arc}°",
                         PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
-                else if (power1.Radius > 0.0)
+                }
+                else if (power1.Radius > 0)
+                {
                     iPopup.Sections[index2].Add("Radius:", PopUp.Colors.Title,
-                        Convert.ToString(power1.Radius, CultureInfo.InvariantCulture) + "ft", PopUp.Colors.Title, 0.9f,
+                        $"{power1.Radius}ft", PopUp.Colors.Title, 0.9f,
                         FontStyle.Bold, 1);
-                if (power1.CastTime > 0.0)
+                }
+
+                if (power1.CastTime > 0)
+                {
                     iPopup.Sections[index2].Add("Cast Time:", PopUp.Colors.Title,
-                        Utilities.FixDP(power1.CastTime) + "s", PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
-                var power2 = power1;
-                if (power2.Effects.Length > 0)
+                        $"{Utilities.FixDP(power1.CastTime)}s", PopUp.Colors.Title, 0.9f, FontStyle.Bold, 1);
+                }
+
+                if (power1.Effects.Length > 0)
                 {
                     iPopup.Sections[index2].Add("Effects:", PopUp.Colors.Title);
-                    char[] chArray = {'^'};
-                    var num1 = power2.Effects.Length - 1;
-                    for (var index3 = 0; index3 <= num1; ++index3)
+                    foreach (var fx in power1.Effects)
                     {
                         var index4 = iPopup.Add();
-                        power1.Effects[index3].SetPower(power1);
-                        var strArray = power1.Effects[index3].BuildEffectString().Replace("[", "\r\n")
-                            .Replace("\r\n", "^").Replace("  ", string.Empty).Replace("]", string.Empty).Split(chArray);
-                        var num2 = strArray.Length - 1;
-                        for (var index5 = 0; index5 <= num2; ++index5)
+                        fx.SetPower(power1);
+                        var strArray = fx.BuildEffectString()
+                            .Replace("[", "\r\n")
+                            .Replace("\r\n", "^")
+                            .Replace("  ", string.Empty)
+                            .Replace("]", string.Empty)
+                            .Split('^');
+                        for (var index5 = 0; index5 < strArray.Length; index5++)
+                        {
                             if (index5 == 0)
-                                iPopup.Sections[index4].Add(strArray[index5], PopUp.Colors.Effect, 0.9f, FontStyle.Bold,
-                                    1);
+                            {
+                                iPopup.Sections[index4].Add(strArray[index5], PopUp.Colors.Effect, 0.9f, FontStyle.Bold, 1);
+                            }
                             else
-                                iPopup.Sections[index4].Add(strArray[index5], PopUp.Colors.Disabled, 0.9f,
-                                    FontStyle.Italic, 2);
+                            {
+                                iPopup.Sections[index4].Add(strArray[index5], PopUp.Colors.Disabled, 0.9f, FontStyle.Italic, 2);
+                            }
+                        }
                     }
                 }
 
@@ -385,7 +402,10 @@ namespace Mids_Reborn.Forms.WindowMenuItems
         private void PopInfo_MouseEnter(object sender, EventArgs e)
         {
             if (!ContainsFocus)
+            {
                 return;
+            }
+
             VScrollBar1.Focus();
         }
 
@@ -407,7 +427,7 @@ namespace Mids_Reborn.Forms.WindowMenuItems
         {
             iList.UpdateTextColors(ListLabel.LlItemState.Enabled, MidsContext.Config.RtFont.ColorPowerAvailable);
             iList.UpdateTextColors(ListLabel.LlItemState.Disabled, MidsContext.Config.RtFont.ColorPowerDisabled);
-            iList.UpdateTextColors(ListLabel.LlItemState.Invalid, Color.FromArgb(byte.MaxValue, 0, 0));
+            iList.UpdateTextColors(ListLabel.LlItemState.Invalid, Color.FromArgb(0xFF, 0, 0));
             iList.ScrollBarColor = MidsContext.Character.IsHero()
                 ? MidsContext.Config.RtFont.ColorPowerTakenHero
                 : MidsContext.Config.RtFont.ColorPowerTakenVillain;
@@ -433,9 +453,11 @@ namespace Mids_Reborn.Forms.WindowMenuItems
         private void VScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
             if (VScrollBar1.Value == -1)
+            {
                 return;
-            PopInfo.ScrollY = (float) (VScrollBar1.Value / (double) (VScrollBar1.Maximum - VScrollBar1.LargeChange) *
-                                       (PopInfo.lHeight - (double) Panel1.Height));
+            }
+
+            PopInfo.ScrollY = VScrollBar1.Value / (float) (VScrollBar1.Maximum - VScrollBar1.LargeChange) * (PopInfo.lHeight - Panel1.Height);
         }
     }
 }
