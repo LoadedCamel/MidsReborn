e8f80b58a3306177251746f30bf5c350491d10d2
zethriller <thriller.ze@gmail.com>
2025-11-09 05:37:26 +0100

    Bump version to 3.7.15 rev 1

--- MidsReborn/Core/Base/Master_Classes/MidsContext.cs
+++ MidsReborn/Core/Base/Master_Classes/MidsContext.cs
@@ -8,10 +8,10 @@ namespace Mids_Reborn.Core.Base.Master_Classes
         public const string AppName = "Mids Reborn";
         private const int AppMajorVersion = 3;
         private const int AppMinorVersion = 7;
-        private const int AppBuildVersion = 14;
-        private const int AppRevisionVersion = 4;
-        public const string AssemblyVersion = "3.7.14";
-        public const string AssemblyFileVersion = "3.7.14.4";
+        private const int AppBuildVersion = 15;
+        private const int AppRevisionVersion = 1;
+        public const string AssemblyVersion = "3.7.15";
+        public const string AssemblyFileVersion = "3.7.15.1";
       
         public static Version AppFileVersion { get; set; } = new(AppMajorVersion, AppMinorVersion, AppBuildVersion, AppRevisionVersion);
 
