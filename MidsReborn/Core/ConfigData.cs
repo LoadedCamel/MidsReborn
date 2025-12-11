0059efb6c21be8bc21a616bed90f375602fbaea4
zethriller <thriller.ze@gmail.com>
2025-12-11 12:43:09 +0100

    Build Templates #2 - Config changes

--- MidsReborn/Core/ConfigData.cs
+++ MidsReborn/Core/ConfigData.cs
@@ -171,6 +171,8 @@ namespace Mids_Reborn.Core
         public Enums.WordwrapMode PowerListsWordwrapMode { get; set; }
 
         public CombatContext CombatContextSettings { get; set; }
+        public List<BuildTemplate>? Templates { get; set; } = [];
+        public string? ActiveTemplate { get; set; } = null;
 
         internal bool MasterMode
         {
