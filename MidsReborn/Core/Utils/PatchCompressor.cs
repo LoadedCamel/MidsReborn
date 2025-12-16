19787dc25d4e29639b975a39ce87e6176bed14f8
zethriller <thriller.ze@gmail.com>
2025-12-16 22:03:30 +0100

    Exclude MRBBootstrap* from patch container generation

--- MidsReborn/Core/Utils/PatchCompressor.cs
+++ MidsReborn/Core/Utils/PatchCompressor.cs
@@ -164,7 +164,7 @@ namespace Mids_Reborn.Core.Utils
             {
                 PatchType.Application =>
                 [
-                    "Patches", "Databases", ".pdb", "MidsReborn.exe.WebView2", "appSettings",
+                    "Patches", "Databases", "MRBBootstrap", ".pdb", "MidsReborn.exe.WebView2", "appSettings",
                     $"{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}",
                     $"{Path.DirectorySeparatorChar}Images{Path.DirectorySeparatorChar}",
                     $"{Path.DirectorySeparatorChar}Staging{Path.DirectorySeparatorChar}",
